eaef2fe00a0b917dda4553ba72db878c3d8020a1
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-09-16 23:16:39 +0300

    Updates

---
+++ precache-manifest.01c7260998617144c738250b1efef474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb38e59a5330ba0947bc90ca9356f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c4d1de91e529b205cd4",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "cf34a18abdc32571d1b6",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "9c4d1de91e529b205cd4",
+    "url": "./static/js/2.99d85d3a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.99d85d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf34a18abdc32571d1b6",
+    "url": "./static/js/main.f5b1296f.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
